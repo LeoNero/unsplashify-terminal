bd55aceab245691704d9a46af3cb681d5353f721
Thor Garcia <thor.fc21@gmail.com>
2016-12-28 21:04:31 -0200

    starts dev

--- index.js
+++ index.js
@@ -4,10 +4,15 @@
 
 const request = require('request');
 const wallpaper = require('wallpaper');
+const fs = require('fs');
 
-const URL = 'https://unsplash.com/photos/';
+const URL = 'https://unsplash.com/?photo=';
 
 let args = process.argv;
 
 let photoId = args[2];
+
+request.get(URL + photoId).on('error', function(err) {
+    console.log(err);
+  }).pipe(fs.createWriteStream(photoId + '.png'));
 
