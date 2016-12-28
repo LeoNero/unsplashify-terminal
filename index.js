4a3b9765b75594a558c4be0b37eccae7122d64ef
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2016-12-28 20:51:30 -0200

    Starting project

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const request = require('request');
+const wallpaper = require('wallpaper');
+
+const URL = 'https://unsplash.com/photos/';
+
+let args = process.argv;
+
+let photoId = args[2];
+
