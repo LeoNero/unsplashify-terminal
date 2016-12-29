1888844c8cca81e77d2071226fbb2e8edd114028
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2016-12-28 22:09:14 -0200

    Download progress now working

--- index.js
+++ index.js
@@ -15,24 +15,50 @@ let args = process.argv;
 let photoId = args[2];
 let downloadURL = URL + photoId + '/download';
 
-let stream = fs.createWriteStream(dir + photoId)
-  .on('info', info => {
+let totalBytes = 0;
+let receivedBytes = 0;
 
-  })
-  .on('data', data => {
-    // MOSTRAR PORCENTAGEM DE DOWNLOAD 
-  })
-  .on('finish', () => {
-    console.log("Download finished");
-    setWallpaper();
+downloadPhoto();
+
+function downloadPhoto() {
+  let stream = fs.createWriteStream(dir + photoId)
+    .on('finish', () => {
+      console.log("Download finished");
+      setWallpaper();
+    });
+
+  let requestPhoto = request({
+    method: 'GET',
+    uri: downloadURL
+  });
+  
+  requestPhoto.pipe(stream);
+
+  requestPhoto.on('response', data => {
+    totalBytes = parseInt(data.headers['content-length' ]);
   });
 
-request.get(downloadURL)
-  .pipe(stream)
-  .on('error', err => {
-    console.log('Erro ao fazer download: ' + err);
-});
+  requestPhoto.on('data', chunk => {
+    receivedBytes += chunk.length;
+    showDownloadProgress(receivedBytes, totalBytes);
+  });
 
+  requestPhoto.on('error', err => {
+    console.log("Error while downloading: " + err);
+  })
+}
+
+let oldPercentage = 0;
+ 
+function showDownloadProgress(received, total) {
+  let percentage = (received * 100) / total;
+  let percentageRounded = Math.round(percentage);
+  
+  if (percentageRounded !== oldPercentage) {
+    console.log(`Download ${percentageRounded}%`);
+    oldPercentage = percentageRounded;
+  }
+}
 
 function setWallpaper() {
   wallpaper.set(dir + photoId) 
